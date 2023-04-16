9c7f5ead7f61976a573e84086e3b6ff197f50527
cs20b041 <cs20b041@iittp.ac.in>
2023-04-16 18:18:58 +0530

    UI updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,7 @@ export default function Navbar(props) {
 					<div className="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 							<li className="nav-item">
-								<a className="nav-link active" aria-current="page" href="/codesummarizer">
+								<a className="nav-link " aria-current="page" href="/codesummarizer">
 									Code Summarizer
 								</a>
 								{/* <Link to="/">Code Summarizer</Link> */}
